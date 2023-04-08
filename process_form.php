a0028020a20f1e23264f68089b6ff2d7f91d41c4
samet-byte <sametbayat@hacettepe.edu.tr>
2023-04-08 18:24:46 +0300

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,27 +1,31 @@
 <?php
-// Get the user's response from the form submission
-$user_response = $_POST['g-recaptcha-response'];
+// Verify reCAPTCHA response
+$secretKey = "6Lf86m0lAAAAAOBHw5geHXIUiHf4nB682QWQ_Np9";
+$response = $_POST['g-recaptcha-response'];
+$remoteIP = $_SERVER['REMOTE_ADDR'];
+$url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$response."&remoteip=".$remoteIP;
 
-// Send a POST request to the reCAPTCHA verification API
-$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Lf86m0lAAAAAOBHw5geHXIUiHf4nB682QWQ_Np9&response=".$user_response);
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($curl);
+curl_close($curl);
 
-// Decode the response from the API
-$response_data = json_decode($response);
-
-// Check if the response is valid
-if ($response_data->success) {
-  // The user is not a robot, process the form data
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-
-  // Do something with the form data, such as send an email or store in a database
-  // ...
-
-  // Redirect the user to a thank you page
-  header("Location: thank_you.html");
-} else {
-  // The user is a robot, show an error message
-  echo "Sorry, we could not verify that you are human.";
+$result = json_decode($response, true);
+if ($result['success'] != 1) {
+    // reCAPTCHA validation failed, handle the error
+    // You could display an error message to the user or log the error
+    exit("reCAPTCHA validation failed");
 }
+
+// If reCAPTCHA validation succeeded, process the form data
+$name = $_POST['Name'];
+$email = $_POST['Email'];
+$message = $_POST['Message'];
+
+// Process the form data as needed (send email, save to database, etc.)
+
+// Redirect the user to a thank you page
+header("Location: thank_you.html");
+exit();
 ?>
