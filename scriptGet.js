8b6c1d75981cddaf848eda67c826d06cbbea3b19
samet-byte <sametbayat@hacettepe.edu.tr>
2023-10-03 13:22:45 +0300

    get drive

---
+++ scriptGet.js
@@ -0,0 +1,30 @@
+const API_URL  = 
+'https://script.google.com/macros/s/AKfycbzVxdHo28g9yu07eEgZL9oNadw88Jxe5p2dHUnUeVF9Reyi0uONjKR4RlvK6byArCQehw/exec';
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(API_URL+'')
+      .then(response => response.text())
+      .then(data => {
+        // Split the data into separate lines
+        const lines = data.split('\n');
+  
+        // Create an array of HTML elements for each line
+        const lineElements = lines.map(line => {
+          const lineElement = document.createElement('div');
+          lineElement.textContent = line;
+          return lineElement;
+        });
+  
+        // Get the container element where the lines will be displayed
+        const resultContainer = document.getElementById('result');
+  
+        // Append each line element to the container
+        lineElements.forEach(lineElement => {
+          resultContainer.appendChild(lineElement);
+        });
+      })
+      .catch(error => {
+        // Handle any errors
+        console.error('Error:', error);
+      });
+  });
+  
